08b3a40ebeb5db244599bf2c934b28b591062605
Kaue Correa Colling <kauecoficial@gmail.com>
2024-02-09 18:40:51 -0300

    tomara

---
+++ serviceWorker.js
@@ -0,0 +1,35 @@
+var cacheTeste = 'teste-v1';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheTeste).then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/images/icon.ico',
+                '/FotoPerfil.jpg',
+                '/vite.svg',
+                '/manifest.json',
+                '/serviceWorker.js',
+                '/src/components/Livro/index.jsx',
+                '/src/components/Livro/livro.css',
+                '/src/components/Config/index.jsx',
+                '/src/components/Config/config.css',
+                '/src/components/Pefil/index.jsx',
+                '/src/components/Perfil/iperfil.css',
+                '/src/components/App.css',
+                '/src/components/App.jsx',
+                '/src/components/index.css',
+                '/src/components/main.jsx'
+            ]);
+        })
+    )
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cachedResponse) {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
